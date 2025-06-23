01e491d6af79976b716d266caa2aba9237ca61d8
Kenechukwu Okoh <kenechukwuokoh30@gmail.com>
2025-06-23 16:05:50 +0100

    added clerk middleware

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,28 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from 'express';
+import { clerkMiddleware, requireAuth } from '@clerk/express';
 
-import { errorHandler } from "./middlewares/errorHandler";
-import ratelimiter from "./middlewares/rateLimiter";
+import { errorHandler } from './middlewares/errorHandler';
+import ratelimiter from './middlewares/rateLimiter';
 
-import itemRoutes from "./routes/item.route";
-import userRoutes from "./routes/user.route";
+import itemRoutes from './routes/item.route';
+import userRoutes from './routes/user.route';
 
 const app = express();
 
+app.use(clerkMiddleware());
 app.use(ratelimiter);
 app.use(express.json());
 
 // Routes
-app.use("/api/items", itemRoutes);
-app.use("/api/users", userRoutes);
+app.use('/api/items', itemRoutes);
+app.use('/api/users', userRoutes);
 
-app.get("/api/health", (req, res) => {
-  res.status(200).json({ message: "Healthy" });
+app.get('/api/health', (req, res) => {
+  res.status(200).json({ message: 'Healthy' });
+});
+
+app.get('/api/protected', requireAuth(), (req, res) => {
+  res.send('This is a protected route');
 });
 
 app.use(errorHandler);
